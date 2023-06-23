73c986494d08ec60ad82ef9568360b4fe61751b2
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-23 11:36:30 -0300

    Hello World funcionou

---
+++ ProducerExample/ProducerExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost", UserName = "admin", Password = "admin" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare("hello", false, false, false, null);
+
+const string message = "Hello World";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(string.Empty, "hello", null, body);
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine("Press enter to exit.");
+Console.ReadLine();
